4f41561b11d48019f2284267ebfc4e97d09d25a4
Joel <joel.wejdenstal@gmail.com>
2022-05-05 21:42:57 +0200

    Don't enforce standard k8s and ssh auth mechanisms when joining sessions (#11144)

--- tool/tsh/tsh.go
+++ tool/tsh/tsh.go
@@ -1990,6 +1990,7 @@ func onJoin(cf *CLIConf) error {
 		return trace.Wrap(err)
 	}
 
+	cf.NodeLogin = teleport.SSHSessionJoinPrincipal
 	tc, err := makeClient(cf, true)
 	if err != nil {
 		return trace.Wrap(err)
